770c92bcaae5c2863e707d36ce8648c2f3b74913
Machnev Vassyl <pustotasliwkomholodna@gmail.com>
2018-04-13 16:24:08 +0300

    adding files

---
+++ js/animate.js
@@ -0,0 +1,32 @@
+
+    window.sr = ScrollReveal();
+    sr.reveal('.left-menu ', {
+      duration: 4000,
+      origin: 'bottom'
+    });
+    sr.reveal('.image-container', {
+      duration: 4500,
+      origin: 'right',
+      distance: '300px'
+    });
+    sr.reveal('.grid', {
+      duration: 3500,
+      origin: 'left',
+      distance: '300px',
+      viewFactor: 0.2
+      
+    });
+    sr.reveal('#footer', {
+      duration: 2000,
+      delay: 1200,
+      origin: 'left',
+      distance:'300px',
+      viewFactor:0.2
+  });
+    sr.reveal('#skillset', {
+      duration: 3000,
+      origin: 'left',
+      distance: '300px',
+      viewFactor: 0.4
+    });
+   
